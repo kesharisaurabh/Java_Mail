754560e9099f217affb235c5d73d1d49de3a7187
SAURABH KESHARI <keshari.saurabh@yahoo.in>
2020-02-05 15:01:34 +0530

    Add files via upload

---
+++ java_Mail_1/src/pkg/javaMail.java
@@ -0,0 +1,12 @@
+package pkg;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+public class javaMail 
+{
+	public static void main(String[] args) throws Exception 
+	{
+		java_Mail_Box_1.sendEmail("saurabh4321keshari@gmail.com");
+	}
+}
